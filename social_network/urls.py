2602b7421426119f4bf9550d153f70cd30d5f7d0
ProgammerMasya <vlad3032001@mail.ru>
2019-04-08 16:50:41 +0300

    JWT

--- social_network/urls.py
+++ social_network/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from .router import router
 
@@ -22,4 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("api/", include(router.urls)),
+    path("api-token-auth/", obtain_jwt_token),
+    path("api-token-refresh/", refresh_jwt_token),
+    path("api-token-verify/", verify_jwt_token),
 ]
